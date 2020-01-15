080ad39867ae22a973df5465d1817ab4aab24d74
Ramzan Chasygov <theliketurbo@gmail.com>
2020-01-16 02:52:32 +0300

    Added typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: 'Продажа людей, бананов...',
+    title: "Продажа людей, бананов...",
     description:
-      'На этом сайте вы cможете купить людей и бананы по самой выгодной цене.',
+      "На этом сайте вы cможете купить людей и бананы по самой выгодной цене.",
     author: `@liketurbo`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-material-ui',
+      resolve: "gatsby-plugin-material-ui",
       options: {
         stylesProvider: {
           injectFirst: true,
         },
       },
     },
+    "gatsby-plugin-typescript",
   ],
 }
